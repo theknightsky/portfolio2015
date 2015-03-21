8451517cedddf6261412b68ca77439ae020c71c7
Skyler Knight <skyler.knight22@gmail.com>
2015-03-21 21:58:00 +0900

    Added authentication data for mail permissions

--- app/config/mail.php
+++ app/config/mail.php
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => 'aqlsjjtzxrxbdtqu',
+	'password' => 'wxglavvacjrxiigt',
 
 	/*
 	|--------------------------------------------------------------------------
